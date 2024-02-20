f457e3733e42ad1a3bfb2d7275628a7aa7247dc1
Viner108 <andrukovichl@mail.ru>
2024-02-20 15:15:42 +0300

    сцена

--- src/main/java/tank/TankApplication.java
+++ src/main/java/tank/TankApplication.java
@@ -3,10 +3,13 @@ package tank;
 
 import tank.view.Scene;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class TankApplication {
 
     public static void main(String[] args) {
-        Scene scene=new Scene();
+        Scene scene =new Scene();
     }
 
 
--- src/main/java/tank/view/Scene.java
+++ src/main/java/tank/view/Scene.java
@@ -5,22 +5,18 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Scene extends JFrame {
-
-    public Scene(){
-        setTitle("Мое окно");
-
-
-        setSize(500, 400);
-
-        JButton button = new JButton("Нажми меня");
+    public Scene() {
+        setTitle("Танки");
+        getContentPane().setBackground(new Color(34, 139, 34));
 
 
-        add(button);
-
+        setSize(1000, 1000);
 
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         setVisible(true);
     }
+
+
 }
