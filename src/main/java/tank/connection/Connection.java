c28f23a43f2bed35f7f47744a262acd42f4d7eba
Viner108 <andrukovichl@mail.ru>
2024-02-25 16:25:12 +0300

    возможность переподключения клиента к серверу

--- src/main/java/tank/connection/Connection.java
+++ src/main/java/tank/connection/Connection.java
@@ -15,37 +15,13 @@ public class Connection extends Thread {
     private static int PORT = 8001;
     private boolean isConnected = false;
     private Socket socketOut;
-    private OutputStream out;
-    private ObjectOutputStream oos;
 
     @Override
     public void run() {
         startConnection();
         while (true) {
-            try {
-                out = socketOut.getOutputStream();
-                oos = new ObjectOutputStream(out);
-                DataOutputStream dataOutputStream = new DataOutputStream(out);
-                String string = "run";
-                dataOutputStream.writeUTF(string);
-            } catch (Exception e) {
-                e.printStackTrace();
-                try {
-                    isConnected = false;
-                    Thread.sleep(5000);
-                    startConnection();
-                } catch (InterruptedException ex) {
-                    e.printStackTrace();
-                }
-            }
+
         }
-//        try {
-//            while (!isConnected) {
-//                closeConnection();
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
 
     public void startConnection() {
@@ -54,6 +30,7 @@ public class Connection extends Thread {
                 System.out.println("Start");
                 socketOut = new Socket(HOST, PORT);
                 isConnected = true;
+                System.out.println("Connection establishment");
             } catch (Exception e) {
                 e.printStackTrace();
                 try {
@@ -63,9 +40,5 @@ public class Connection extends Thread {
                 }
             }
         }
-    }
-
-    public void closeConnection() throws IOException {
-        socketOut.close();
     }
 }
